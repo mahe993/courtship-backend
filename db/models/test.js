19848c4a03709fe347f72aa40bb0e5953fb44532
Ma He <qmahe@live.com>
2022-10-20 00:51:39 +0800

    change sequelizecli in node_modules to generate es6 when using model:generate

---
+++ db/models/test.js
@@ -0,0 +1,23 @@
+'use strict';
+import {
+  Model
+} from "sequelize"
+export default (sequelize, DataTypes) => {
+  class test extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  test.init({
+    name: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'test',
+  });
+  return test;
+};
