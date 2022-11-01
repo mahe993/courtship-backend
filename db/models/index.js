aae64e936ab66884d38ba9edec918ac1f7e59657
Ma He <qmahe@live.com>
2022-11-01 20:35:45 +0800

    config

--- db/models/index.js
+++ db/models/index.js
@@ -12,8 +12,11 @@ const basename = path.basename(__filename);
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (config[process.env.NODE_ENV].use_env_variable) {
+  sequelize = new Sequelize(
+    config[process.env.NODE_ENV].use_env_variable,
+    config[process.env.NODE_ENV]
+  );
 } else {
   sequelize = new Sequelize(
     config.development.database,
